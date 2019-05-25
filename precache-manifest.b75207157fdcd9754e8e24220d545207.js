234623a0dd3ffb21b86017a5c3d5e8fd49d0d0d2
Andrii Maksymov <echoes@ex.ua>
2019-05-25 13:04:14 +0300

    Updates

---
+++ precache-manifest.b75207157fdcd9754e8e24220d545207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0208fe78dfe7ef4b95b77aed1dce0048",
+    "url": "/anonymous-chat/index.html"
+  },
+  {
+    "revision": "56f11e5ce31741b798a8",
+    "url": "/anonymous-chat/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/anonymous-chat/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "56f11e5ce31741b798a8",
+    "url": "/anonymous-chat/static/js/main.6f4a43c5.chunk.js"
+  },
+  {
+    "revision": "7252fa85428bc4ee98ec",
+    "url": "/anonymous-chat/static/js/runtime~main.fb024221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anonymous-chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
